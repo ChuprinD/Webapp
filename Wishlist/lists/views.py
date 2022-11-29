b422f77f9ce6a2abaed25615456d78be34467865
ChuprinD <chuprin2005@yandex.ru>
2022-11-30 00:46:36 +0300

    added logout

--- Wishlist/lists/views.py
+++ Wishlist/lists/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'lists/index.html')
+    #return HttpResponse("Hello, world. You're at the polls index.")
 
